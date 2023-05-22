6405452cf50fe62db45cbea3c00face5b4be47ac
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-05-22 11:51:56 +0100

    New index, temp index with countdown. New pages with info.

--- index.php
+++ index.php
@@ -1,156 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://kit.fontawesome.com/059448ac20.js" crossorigin="anonymous"></script>
-<style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  margin: 0;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-background-image: url('/src/img/background.jpg');
+<!-- Display the countdown timer in an element -->
+<p id="demo"></p>
 
-}
-*, ::after, ::before{
-  box-sizing: inherit;
-}
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 12, 2024 12:00:00").getTime();
 
-body::-webkit-scrollbar {
-  display: none;
-}
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-img{
-  float: left;
-  position: relative;
-  block-size: 3em;
-  margin-left: 18px;
-  margin-right:24px;
-}
+  // Get today's date and time
+  var now = new Date().getTime();
 
-.navbar {
-  overflow: hidden;
-  background-color: rgb(0, 0, 0);
-}
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-.navbar a {
-  float: left;
-  font-size: 16px;
-  color: white;
-  text-align: center;
-  padding: 24px 24px;
-  text-decoration: none;
-}
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-.subnav {
-  float: left;
-  overflow: hidden;
-  display: block;
-}
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
 
-.subnav .subnavbtn {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 24px 24px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-.navbar a:hover, .subnav:hover .subnavbtn {
-  background-color: #F36F21;
-}
-
-.subnav-content {
-  min-width: 120px;
-  display: none;
-  position: absolute;
-  left: 1;
-  background-color: rgb(65, 65, 65);
-  z-index: 1;
-}
-
-.subnav-content a {
-  min-width: 120px;
-  text-align: left;
-  float: left 15px;
-  color: white;
-  text-decoration: none;
-  display: block;
-}
-
-.subnav-content a:hover {
-  background-color: #F36F21;
-  color: white;
-}
-
-.subnav:hover .subnav-content {
-  display: flex;
-  flex-direction: column;
-}
-
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-.column{
-  float: left;
-  width: 48%;
-  padding: 10px;
-  height: 300px;
-}
-#purple{
-    border: 0.5em solid #3A3684;
-    margin: 1em;
-    padding: 1em;
-    border-radius: 1em;
-    background: rgba(255, 255, 255, 0.9);
-    text-align: center;
-    height: 100%;
-}
-#nolink{
-  color: black;
-  text-decoration: none;
-}
-#nolink:hover{
-  color: #F36F21;
-  text-decoration: none;
-}
-
-
-</style>
-</head>
-<body>
-<div class="navbar">
-		<img href="https://octopus.jadith.co.uk/" src="/src/img/sar.png" alt="Octopus - The new project">
-</div>
-
-<div>
-    <div class="row">
-        <div class="column">
-            <div id="purple">
-                <h1>Octo 1</h1>
-                <br />
-                <p>test</p>
-                <br />
-                <h2><a id="nolink" href="./customer/validation/login.php"> Go to our Customer Portal <i class="fa-solid fa-circle-arrow-right" ></i></a></h2>
-              </div>
-        </div>
-        <div class="column">
-            <div id="purple">
-                <h1>Octo 2</h1>
-                <br />
-                <p>Test</p>
-                <br />
-                <h2 ><a id="nolink" href="./customer/validation/login.php"> Enter the Volunteer Network <i class="fa-solid fa-circle-arrow-right" ></i></a></h2>
-              </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
-</html>
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
