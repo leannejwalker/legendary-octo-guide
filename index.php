9e955b5002807547f65fdeabc921f255971224f5
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-05-22 11:28:28 +0100

    Confirm "index.php"

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<?php
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-// Initialize the session
-session_start();
-require_once "scripts/config.php";
-
-// Login Page
-ob_start();
-include "validation/login.php";
-$loginpage = ob_get_contents(); ob_end_clean();
-
-//Logged In
-ob_start();
-include "account.php";
-$loggedin = ob_get_contents(); ob_end_clean();
-=======
 <!DOCTYPE html>
 <html>
 <head>
@@ -36,7 +19,6 @@ background-image: url('/src/img/background.jpg');
 body::-webkit-scrollbar {
   display: none;
 }
->>>>>>> parent of 25d2de6 (Reorder files, making customer page priority)
 
 img{
   float: left;
