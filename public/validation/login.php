9f5db955a73077b9f796e4b7cc79b20b3060586a
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-10-18 11:06:54 +0100

    .

--- public/validation/login.php
+++ public/validation/login.php
@@ -70,7 +70,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;
                             $_SESSION["access_id"] = $access_id;                           
                         
-                                header("location: $_SERVER['DOCUMENT_ROOT'] . account.php");
+                                header("$_SERVER['DOCUMENT_ROOT'] . /public/account.php");
 
                         } else{
                             // Password is not valid, display a generic error message
