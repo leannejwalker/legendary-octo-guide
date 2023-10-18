1c3904733a33e32c2126cdc801685b07636384df
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-10-18 11:33:14 +0100

    ,

--- public/validation/login.php
+++ public/validation/login.php
@@ -3,9 +3,6 @@
 // Display errors
 ini_set('display_errors', 1);
 
-// Initialize the session
-session_start();
- 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: dashboard.php");
