2c608b36d47f376436bbcc8921917fabc5352c14
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-10-18 10:00:14 +0100

    .

--- public/validation/login.php
+++ public/validation/login.php
@@ -13,7 +13,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
  
 // Include config file
-require_once($_SERVER['DOCUMENT_ROOT'] ."src/config.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] .'src/config.php');
 
 
 // Define variables and initialize with empty values
