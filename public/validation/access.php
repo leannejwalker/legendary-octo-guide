9dd6f7c2b28bc99a6d839e3c7d3423e6dff5d796
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2023-10-27 12:03:27 +0100

    a

--- public/validation/access.php
+++ public/validation/access.php
@@ -1,50 +1,3 @@
-<?php
-session_start(); // Start the session
-
-// Include config file
-require_once $_SERVER['DOCUMENT_ROOT'] . "/src/config.php";
-
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: dashboard.php");
-    exit;
-}
-
-// Define variables and initialize with empty values
-$email = "";
-$email_err = "";
-$show_password_fields = false; // Flag to determine whether to show password fields
-
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Check if the email is entered
-    if (empty(trim($_POST["email"]))) {
-        $email_err = "Please enter an email.";
-    } else {
-        $email = trim($_POST["email"]);
-        // Check if the email exists in the database
-        $sql = "SELECT id FROM users WHERE email = ?";
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_email);
-            // Set parameters
-            $param_email = $email;
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    $show_password_fields = true; // User exists, show password fields
-                } else {
-                    $show_password_fields = false; // User doesn't exist, show registration fields
-                }
-            }
-            mysqli_stmt_close($stmt);
-        }
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -86,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             border-color: #3A3684;
         }
 
-        .registration-form { /* Style for registration form elements */
+        .registration-form {
             display: none;
         }
     </style>
@@ -96,42 +49,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="wrapper">
     <h2>Sign Up / Login</h2>
     <p>Please enter your email to continue.</p>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <a style="color:red;">*</a>Email Address: <input type="text" name="email"
+    <form id="email-form">
+        <a style="color:red;">*</a>Email Address: <input type="text" id="email" name="email"
                                                       pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required/><br/>
-        <button type="button" onclick="toggleForm();">Continue</button>
-        <div class="registration-form" <?php echo $show_password_fields ? 'style="display: block;"' : ''; ?>>
+        <button type="button" onclick="checkEmail();">Continue</button>
+        <div class="registration-form" style="display: none;">
             <div class="form-group">
                 <label>Password</label>
                 <input type="password" name="password"
-                       class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                       class="form-control"
                        value=""
                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
                        required>
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password"
-                       class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
+                       class="form-control"
                        value="">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary"
-                   value="<?php echo $show_password_fields ? 'Login' : 'Register'; ?>">
+            <input type="submit" class="btn btn-primary" id="register" value="Register">
+            <input type="submit" class="btn btn-primary" id="login" value="Login">
         </div>
-        <p><?php echo $show_password_fields ? 'Already have an account? <a href="login.php">Login here</a>.' : 'Don\'t have an account? <a href="register.php">Register here</a>.'; ?></p>
+        <p>Don't have an account? <a href="register.php">Register here</a>.</p>
     </form>
 </div>
 
 <script>
-    function toggleForm() {
+    function checkEmail() {
+        const email = document.getElementById('email').value;
         const registrationForm = document.querySelector('.registration-form');
-        registrationForm.style.display = registrationForm.style.display === 'none' ? 'block' : 'none';
+        const loginButton = document.getElementById('login');
+        const registerButton = document.getElementById('register');
+
+        // Send an AJAX request to check if the email exists
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'check_email.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.exists) {
+                    registrationForm.style.display = 'none';
+                    loginButton.style.display = 'block';
+                    registerButton.style.display = 'none';
+                } else {
+                    registrationForm.style.display = 'block';
+                    loginButton.style.display = 'none';
+                    registerButton.style.display = 'block';
+                }
+            }
+        };
+        xhr.send('email=' + email);
     }
 </script>
 </body>
 </html>
+
