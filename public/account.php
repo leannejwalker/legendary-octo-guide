691c2d9d651be1e049e69b7fa6322892583fb1bb
leannejwalker <jadewalker2122@gmail.com>
2024-02-23 20:16:38 +0000

    changes

--- public/account.php
+++ public/account.php
@@ -7,7 +7,7 @@ session_start();
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: /public/validation/login.php");
+    header ("location: /public/validation/login.php");
 }
 
 // Include config file
